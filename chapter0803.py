8e653871678c86665eaba901e30f51b6002beae7
Charlie <ZhipengLi@users.noreply.github.com>
2025-04-21 18:48:08 -0700

    Add files via upload

---
+++ chapter0803.py
@@ -0,0 +1,95 @@
+### training accuracy: 0.841
+
+import os, shutil, pathlib
+from tensorflow import keras 
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+from tensorflow.keras.utils import image_dataset_from_directory
+
+original_dir = pathlib.Path("C:\\Users\\zhipli.REDMOND\\Downloads\\dogs-vs-cats\\train")
+new_base_dir = pathlib.Path("cats_vs_dogs_small")
+def make_subset(subset_name, start_index, end_index):  
+    for category in ("cat", "dog"):
+        dir = new_base_dir / subset_name / category
+        os.makedirs(dir)
+        fnames = [f"{category}.{i}.jpg" for i in range(start_index, end_index)]
+        for fname in fnames:
+            shutil.copyfile(src=original_dir / fname,dst=dir / fname)
+make_subset("train", start_index=0, end_index=1000)  
+make_subset("validation", start_index=1000, end_index=1500)   
+make_subset("test", start_index=1500, end_index=2500)
+
+data_augmentation = keras.Sequential(
+    [
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(0.1),
+        layers.RandomZoom(0.2),
+        ]
+    )
+
+
+inputs = keras.Input(shape=(180, 180, 3))
+x = data_augmentation(inputs)
+x = layers.Rescaling(1./255)(x)
+x = layers.Conv2D(filters=32, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=64, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=128, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=256, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=256, kernel_size=3, activation="relu")(x)
+x = layers.Flatten()(x)
+x = layers.Dropout(0.5)(x)
+outputs = layers.Dense(1, activation="sigmoid")(x)
+model = keras.Model(inputs=inputs, outputs=outputs)
+model.compile(loss="binary_crossentropy",
+    optimizer="rmsprop",
+    metrics=["accuracy"])
+
+
+train_dataset = image_dataset_from_directory(
+    new_base_dir / "train",
+    image_size=(180, 180),
+    batch_size=32)
+validation_dataset = image_dataset_from_directory(
+    new_base_dir / "validation",
+    image_size=(180, 180),
+    batch_size=32)
+test_dataset = image_dataset_from_directory(
+    new_base_dir / "test",
+    image_size=(180, 180),
+    batch_size=32)
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint(
+    filepath="convnet_from_scratch_with_augmentation.keras",
+    save_best_only=True,
+    monitor="val_loss")
+    ]
+history = model.fit(
+    train_dataset,
+    epochs=100,
+    validation_data=validation_dataset,
+    callbacks=callbacks)
+
+accuracy = history.history["accuracy"]
+val_accuracy = history.history["val_accuracy"]
+loss = history.history["loss"]
+val_loss = history.history["val_loss"]
+epochs = range(1, len(accuracy) + 1)
+plt.plot(epochs, accuracy, "bo", label="Training accuracy")
+plt.plot(epochs, val_accuracy, "b", label="Validation accuracy")
+plt.title("Training and validation accuracy")
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, "bo", label="Training loss")
+plt.plot(epochs, val_loss, "b", label="Validation loss")
+plt.title("Training and validation loss")
+plt.legend()
+plt.show()
+#print("accuracy:\n",accuracy)
+test_model = keras.models.load_model("convnet_from_scratch_with_augmentation.keras")
+test_loss, test_acc = test_model.evaluate(test_dataset)
+print(f"Test accuracy: {test_acc:.3f}")
